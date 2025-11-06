db31f01511ebeb77301611710a614fb3a0bb936c
DipaliKirdak25 <kirdakdipali58@gmail.com>
2025-11-06 12:17:45 +0530

    Removed ExperienceView references

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView,AboutView,EducationView,ExperienceView,ProjectView,ContactView
+from .views import HomeView,AboutView,EducationView,ProjectView,ContactView
 
 urlpatterns = [
      path('', HomeView.as_view(), name="home"),
