2ff0ed7763859188bea5011f77bec0ccdd3b8a9f
DipaliKirdak25 <kirdakdipali58@gmail.com>
2025-11-07 12:48:38 +0530

    Fix Procfile to use Render port And Fix Allow host

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']  # later replace * with your Render domain
+ALLOWED_HOSTS = ['portfolio-iscf.onrender.com', 'localhost'] # later replace * with your Render domain
 
 # Application definition
 INSTALLED_APPS = [
